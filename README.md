bea527eb11b7546cf6722f57e03b7aa9b4c09dda
v_liyadong <v_liyadong@tal.com>
2021-05-12 19:01:57 +0800

    集中一下

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 welcome to my blog!
+看板娘
+  <script src="https://eqcn.ajz.miesnfu.com/wp-content/plugins/wp-3d-pony/live2dw/lib/L2Dwidget.min.js"></script>
+    <script>
+    var jsonModel = [
+      // "https://unpkg.com/live2d-widget-model-miku@1.0.5/assets/miku.model.json", // 初音
+      "https://unpkg.com/live2d-widget-model-shizuku@1.0.5/assets/shizuku.model.json", // 萌娘
+      "https://unpkg.com/live2d-widget-model-koharu@1.0.5/assets/koharu.model.json", // 小可爱（女）
+      // "https://unpkg.com/live2d-widget-model-haruto@1.0.5/assets/haruto.model.json", // 小可爱（男）
+      // "https://unpkg.com/live2d-widget-model-chitose@1.0.5/assets/chitose.model.json", // 小帅哥
+    ];
+    L2Dwidget.init({
+      "model": {
+        "jsonPath": jsonModel[Math.floor(Math.random() * (jsonModel.length - 1))],
+        "scale": 1
+      },
+      "display": {
+        "position": "right", // 位置left、right
+        "width": 120, // 宽度
+        "height": 150, // 高度
+        "hOffset": 10, // 横向边距
+        "vOffset": 5 // 众向边距
+      },
+      "mobile": {
+        "show": false // 手机是否显示
+      },
+    });
+  </script>
+播放器插件
+  安装
+  <script src="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/meting@2/dist/Meting.min.js"></script>
+  歌单id
+      <!-- //我喜欢的 125102396 -->
+      <!-- //苏联 2005412605 -->
+  配置
+      <!-- 
+        *id
+          song id / playlist id / album id / search keyword
+        *server	
+          平台: netease, tencent, kugou, xiami, baidu
+        *type
+          song, playlist, album, search, artist
+        auto
+        fixed	默认false	
+        mini	默认false	
+        autoplay 默认false
+        theme	默认2980b9
+        loop 默认'all'
+          'all', 'one', 'none'
+        order	默认list
+          'list', 'random'
+        preload	预加载.默认auto
+          'none', 'metadata', 'auto'
+        volume	默认0.7
+        mutex	true 互斥锁
+        lrc-type	默认0	歌词类型
+        list-folded	默认false 列表折叠
+        list-max-height	默认340px
+        storage-name	metingjs
+      -->
---
+++ assets/js/mouseFlower.js
@@ -0,0 +1,165 @@
+
+// 鼠标字体
+function mouseFont (){
+  var arrIndex = 0;
+  var addTips = function (e) {
+    let arrfont = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+    let arrcolor = new Array("#70f3ff", "#ff461f", "#fff143", "#3b2e7e", "#ffb3a7", "#f20c00", "#00e500", "#801dae", "#ffb61e", "#ff0097", "#f05654", "#faff72");
+    let $i = $("<span/>").text(arrfont[arrIndex]);
+    arrIndex = (arrIndex + 1) % arrfont.length;
+    let x = e.pageX,y = e.pageY;
+    $i.css({
+      "z-index": 888888,
+      "top": y - 20,
+      "left": x,
+      "position": "absolute",
+      'font-size':'18px',
+      "font-weight": "800",
+      "color": `${arrcolor[arrIndex]}`
+    });
+    $("body").append($i);
+    $i.animate({
+      "top": y - 150,
+      "opacity": 0
+    },1500,function () {
+        $i.remove();
+    });
+  }
+  $("body").click(addTips);
+  $("body").bind("contextmenu",addTips)
+}
+// 鼠标烟花
+function mouseFlower() {
+  let balls = [];
+  let longPressed = false;
+  let longPress;
+  let multiplier = 0;
+  let width, height;
+  let origin;
+  let normal;
+  let ctx;
+  const colours = ["#F73859", "#14FFEC", "#00E0FF", "#FF99FE", "#FAF15D",'#801dae',"#ff0097"];
+  const canvas = document.createElement("canvas");
+  console.log(document.body);
+  document.body.appendChild(canvas);
+  canvas.setAttribute("style", "width: 100%; height: 100%; top: 0; left: 0; z-index: 888888; position: fixed; pointer-events: none;");
+  const pointer = document.createElement("span");
+  pointer.classList.add("pointer");
+  document.body.appendChild(pointer);
+ 
+  if (canvas.getContext && window.addEventListener) {
+    ctx = canvas.getContext("2d");
+    updateSize();
+    window.addEventListener('resize', updateSize, false);
+    loop();
+    window.addEventListener("mousedown", function(e) {
+      pushBalls(randBetween(10, 20), e.clientX, e.clientY);
+      document.body.classList.add("is-pressed");
+      longPress = setTimeout(function(){
+        document.body.classList.add("is-longpress");
+        longPressed = true;
+      }, 500);
+    }, false);
+    window.addEventListener("mouseup", function(e) {
+      clearInterval(longPress);
+      if (longPressed == true) {
+        document.body.classList.remove("is-longpress");
+        pushBalls(randBetween(50 + Math.ceil(multiplier), 100 + Math.ceil(multiplier)), e.clientX, e.clientY);
+        longPressed = false;
+      }
+      document.body.classList.remove("is-pressed");
+    }, false);
+    window.addEventListener("mousemove", function(e) {
+      let x = e.clientX;
+      let y = e.clientY;
+      pointer.style.top = y + "px";
+      pointer.style.left = x + "px";
+    }, false);
+  } else {
+    console.log("canvas or addEventListener is unsupported!");
+  }
+  function updateSize() {
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = window.innerWidth + 'px';
+    canvas.style.height = window.innerHeight + 'px';
+    ctx.scale(2, 2);
+    width = (canvas.width = window.innerWidth);
+    height = (canvas.height = window.innerHeight);
+    origin = {
+      x: width / 2,
+      y: height / 2
+    };
+    normal = {
+      x: width / 2,
+      y: height / 2
+    };
+  }
+  class Ball {
+    constructor(x = origin.x, y = origin.y) {
+      this.x = x;
+      this.y = y;
+      this.angle = Math.PI * 2 * Math.random();
+      if (longPressed == true) {
+        this.multiplier = randBetween(14 + multiplier, 15 + multiplier);
+      } else {
+        this.multiplier = randBetween(6, 12);
+      }
+      this.vx = (this.multiplier + Math.random() * 0.5) * Math.cos(this.angle);
+      this.vy = (this.multiplier + Math.random() * 0.5) * Math.sin(this.angle);
+      this.r = randBetween(8, 12) + 3 * Math.random();
+      this.color = colours[Math.floor(Math.random() * colours.length)];
+    }
+    update() {
+      this.x += this.vx - normal.x;
+      this.y += this.vy - normal.y;
+      normal.x = -2 / window.innerWidth * Math.sin(this.angle);
+      normal.y = -2 / window.innerHeight * Math.cos(this.angle);
+      this.r -= 0.3;
+      this.vx *= 0.9;
+      this.vy *= 0.9;
+    }
+  }
+ 
+  function pushBalls(count = 1, x = origin.x, y = origin.y) {
+    for (let i = 0; i < count; i++) {
+      balls.push(new Ball(x, y));
+    }
+  }
+ 
+  function randBetween(min, max) {
+    return Math.floor(Math.random() * max) + min;
+  }
+ 
+  function loop() {
+    ctx.fillStyle = "rgba(255, 255, 255, 0)";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < balls.length; i++) {
+      let b = balls[i];
+      if (b.r < 0) continue;
+      ctx.fillStyle = b.color;
+      ctx.beginPath();
+      ctx.arc(b.x, b.y, b.r, 0, Math.PI * 2, false);
+      ctx.fill();
+      b.update();
+    }
+    if (longPressed == true) {
+      multiplier += 0.2;
+    } else if (!longPressed && multiplier >= 0) {
+      multiplier -= 0.4;
+    }
+    removeBall();
+    requestAnimationFrame(loop);
+  }
+ 
+  function removeBall() {
+    for (let i = 0; i < balls.length; i++) {
+      let b = balls[i];
+      if (b.x + b.r < 0 || b.x - b.r > width || b.y + b.r < 0 || b.y - b.r > height || b.r < 0) {
+        balls.splice(i, 1);
+      }
+    }
+  }
+}
+mouseFont();
+mouseFlower();
